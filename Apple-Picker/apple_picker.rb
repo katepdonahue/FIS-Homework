b83ba986171d4afbc9f337e7100d54b5a21f6516
Kate <katepdonahue@gmail.com>
2013-11-19 10:00:31 -0500

    Add apple picker methods using collect and select

--- Apple-Picker/apple_picker.rb
+++ Apple-Picker/apple_picker.rb
@@ -1,11 +1,28 @@
+# def apple_picker(fruit)
+#   apples = []
+#   fruit.each do |x|
+#     if x == "apple"
+#       apples << x
+#     end
+#   end
+#   apples
+# end
+
+# def apple_picker(fruit)
+#   apples = []
+#   fruit.collect do |x|
+#     if x == "apple"
+#       apples << x
+#     end
+#   end
+#   apples
+# end
+
 def apple_picker(fruit)
-  apples = []
-  fruit.each do |x|
-    if x == "apple"
-      apples << x
-    end
-  end
-  apples
+  fruit.select { |x| x == "apple" }
 end
 
 puts apple_picker([2, 3, "apple", 4])
+
+
+
