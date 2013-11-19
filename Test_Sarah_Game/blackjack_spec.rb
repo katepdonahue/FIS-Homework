fd6c1426ffc58f9532c1c3fb1ac2d37607105af6
Kate <katepdonahue@gmail.com>
2013-11-19 11:54:40 -0500

    Add tests to test for all 5 attributes

--- Test_Sarah_Game/blackjack_spec.rb
+++ Test_Sarah_Game/blackjack_spec.rb
@@ -3,8 +3,20 @@ require "./sarah_blackjack.rb"
 describe BlackJack do
 
   describe "#initialize" do
-    it "should create attributes card1 - card4" do
+    it "should create attribute card1" do
       expect(BlackJack.new.card1.to_s).to match_regex(/^[1-9]$|^[1][0-1]$/)
+    end
+    it "should create attribute card2" do
+      expect(BlackJack.new.card2.to_s).to match_regex(/^[1-9]$|^[1][0-1]$/)
+    end
+    it "should create attribute card3" do
+      expect(BlackJack.new.card3.to_s).to match_regex(/^[1-9]$|^[1][0-1]$/)
+    end
+    it "should create attribute card4" do
+      expect(BlackJack.new.card4.to_s).to match_regex(/^[1-9]$|^[1][0-1]$/)
+    end
+    it "should create attribute score" do
+      expect(BlackJack.new.score.to_s).to match_regex(/^[0]$/)
     end
   end
 
