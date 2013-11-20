720d5c244dad2b253e8f9b33535d4678881255eb
Kate <katepdonahue@gmail.com>
2013-11-19 19:39:05 -0500

    Add method def to Array class to make test pass

--- TDD-Iteration/max_solution.rb
+++ TDD-Iteration/max_solution.rb
@@ -2,14 +2,26 @@
 # part I. implement a maximum method that takes and array and returns the largest element of the array
 # part II. reimplement this method as an instance method on the array class
  
-def maximum(arr)
- 
-end
+# def maximum(arr)
+#   biggest = arr[0]
+#   arr.each do |element|
+#     if element > biggest
+#       biggest = element
+#     end
+#   end
+#     biggest
+# end
  
  
 # now rewrite this as a method on the array class
 class Array
   def maximum
- 
+    biggest = self[0]
+    self.each do |element|
+      if element > biggest
+        biggest = element
+      end
+    end
+    biggest
   end
 end
