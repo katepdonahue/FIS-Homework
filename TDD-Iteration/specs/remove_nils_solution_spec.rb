17f06403df348ccc4520ab66ded7246991d4a845
Kate <katepdonahue@gmail.com>
2013-11-19 19:52:50 -0500

    actually write test to fail when error is raised

--- TDD-Iteration/specs/remove_nils_solution_spec.rb
+++ TDD-Iteration/specs/remove_nils_solution_spec.rb
@@ -2,6 +2,6 @@ require_relative "../remove_nils_solution"
 
 describe "#some_method" do
   it "should not raise an error" do
-    expect(some_method).to raise_error
+    expect(some_method).to_not raise_error
   end
 end
