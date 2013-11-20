0da9e19dd7ac64fd1f32beba8e70d795ddec12aa
Kate <katepdonahue@gmail.com>
2013-11-19 19:35:40 -0500

    Add test for maximum method in Array class

--- TDD-Iteration/specs/max_solution_spec.rb
+++ TDD-Iteration/specs/max_solution_spec.rb
@@ -1,7 +1,17 @@
 require_relative "../max_solution"
 
-describe "#maximum" do
-  it "should return the largest element of an array" do
-    expect(maximum([1, 2, 9, 5])).to eq(9)
+# describe "#maximum" do
+#   it "should return the largest element of an array" do
+#     expect(maximum([1, 2, 9, 5])).to eq(9)
+#   end
+
+# end
+
+describe Array do
+  describe "#maximum" do
+    it "should return the largest element of an array" do
+      expect([1, 2, 9, 5].maximum).to eq(9)
+    end
   end
+
 end
