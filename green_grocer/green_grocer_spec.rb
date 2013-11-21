cc23f3d5f8ab1dbf7c60e3f2d49fb689663aad72
Kate <katepdonahue@gmail.com>
2013-11-21 07:48:06 -0500

    Test fails

--- green_grocer/green_grocer_spec.rb
+++ green_grocer/green_grocer_spec.rb
@@ -1,7 +1,7 @@
 require "./green_grocer"
 
-describe "#checkout" do
-  it "should sum the number of each item" do
-    expect(checkout(["AVOCADO", "KALE", "AVOCADO"])).to eq(["AVOCADO", "KALE"])
+describe "#count_cart" do
+  it "should sum the number of each item in the cart and return a hash with the numbers" do
+    expect(count_cart(["AVOCADO", "KALE", "AVOCADO"])).to eq({"AVOCADO" => 2, "KALE" => 1})
   end
 end
