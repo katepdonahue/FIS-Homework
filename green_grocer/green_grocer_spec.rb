0f2c51f22d1bcf06facb5a7a2e10c22e3928d396
Kate <katepdonahue@gmail.com>
2013-11-21 11:10:52 -0500

    Write test for add_counter method, cheat and already have written the method so that it passes

--- green_grocer/green_grocer_spec.rb
+++ green_grocer/green_grocer_spec.rb
@@ -1,5 +1,11 @@
 require "./green_grocer"
 
+describe "#add_counter" do
+  it "should add a :count key with a 0 value to each item hash in the cart array" do
+    expect(add_counter([ {"AVOCADO" => {:price => 3.00, :clearance => true}}, {"AVOCADO" => {:price => 3.00, :clearance => true}}])).to eq([{"AVOCADO" => {:price => 3.00, :clearance => true}, :count => 1}, {"AVOCADO" => {:price => 3.00, :clearance => true}, :count => 1}])
+  end
+end
+
 describe "#count_cart" do
   it "should sum the number of each item-hash in the cart and add the sum as a value to the key :count in that hash" do
     expect(count_cart(add_counter([ {"AVOCADO" => {:price => 3.00, :clearance => true}}, {"AVOCADO" => {:price => 3.00, :clearance => true}}]))).to eq([{"AVOCADO" => {:price => 3.00, :clearance => true}, :count => 2}])
