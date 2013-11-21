3795d38d5d40eecdbb0c0ce79f0737387ed5fb43
Kate <katepdonahue@gmail.com>
2013-11-21 08:01:44 -0500

    Read directions and fix test so that it tests what the method should actually be doing

--- green_grocer/green_grocer.rb
+++ green_grocer/green_grocer.rb
@@ -1,34 +1,34 @@
-# ITEMS = [  {"AVOCADO" => {:price => 3.00, :clearance => true}},
-#           {"KALE" => {:price => 3.00,:clearance => false}},
-#           {"BLACK_BEANS" => {:price => 2.50,:clearance => false}},
-#           {"ALMONDS" => {:price => 9.00, :clearance => false}},
-#           {"TEMPEH" => {:price => 3.00,:clearance => true}},
-#           {"CHEESE" => {:price => 6.50,:clearance => false}},
-#           {"BEER" => {:price => 13.00, :clearance => false}},
-#           {"PEANUTBUTTER" => {:price => 3.00,:clearance => true}},
-#           {"BEETS" => {:price => 2.50,:clearance => false}}]
+ITEMS = [  {"AVOCADO" => {:price => 3.00, :clearance => true}},
+          {"KALE" => {:price => 3.00,:clearance => false}},
+          {"BLACK_BEANS" => {:price => 2.50,:clearance => false}},
+          {"ALMONDS" => {:price => 9.00, :clearance => false}},
+          {"TEMPEH" => {:price => 3.00,:clearance => true}},
+          {"CHEESE" => {:price => 6.50,:clearance => false}},
+          {"BEER" => {:price => 13.00, :clearance => false}},
+          {"PEANUTBUTTER" => {:price => 3.00,:clearance => true}},
+          {"BEETS" => {:price => 2.50,:clearance => false}}]
 
-# COUPS = [  {:item=>"AVOCADO", :num=>2, :cost=>5.00},
-#           {:item=>"BEER", :num=>2, :cost=>20.00},
-#           {:item=>"CHEESE", :num=>3, :cost=>15.00}]
+COUPS = [  {:item=>"AVOCADO", :num=>2, :cost=>5.00},
+          {:item=>"BEER", :num=>2, :cost=>20.00},
+          {:item=>"CHEESE", :num=>3, :cost=>15.00}]
 
-# #randomly generates a cart of items
-# def generateCart
-#   cart = []
-#   rand(20).times do
-#       cart.push(ITEMS.sample)  
-#   end
-#   cart
-# end
+#randomly generates a cart of items
+def generateCart
+  cart = []
+  rand(20).times do
+      cart.push(ITEMS.sample)  
+  end
+  cart
+end
 
-# #randomly generates set of coupons
-# def generateCoups
-#   coups = []
-#   rand(2).times do
-#       coups.push(COUPS.sample)
-#   end
-#   coups
-# end
+#randomly generates set of coupons
+def generateCoups
+  coups = []
+  rand(2).times do
+      coups.push(COUPS.sample)
+  end
+  coups
+end
 
 def count_cart(cart_array)
   cart_hash = {}
--- green_grocer/green_grocer_spec.rb
+++ green_grocer/green_grocer_spec.rb
@@ -1,7 +1,10 @@
 require "./green_grocer"
 
 describe "#count_cart" do
-  it "should sum the number of each item in the cart and return a hash with the numbers" do
-    expect(count_cart(["AVOCADO", "KALE", "AVOCADO"])).to eq({"AVOCADO" => 2, "KALE" => 1})
+  it "should sum the number of each item-hash in the cart and add the sum as a value to the key :count in that hash" do
+    expect(count_cart([ {"AVOCADO" => {:price => 3.00, :clearance => true}}, {"AVOCADO" => {:price => 3.00, :clearance => true}}])).to eq([{"AVOCADO" => {:price => 3.00, :clearance => true}, :count => 2}])
   end
 end
+
+# For example if your cart was [  {"AVOCADO" => {:price => 3.00, :clearance => true}}, {"AVOCADO" => {:price => 3.00, :clearance => true}}]
+#   it would become {"AVOCADO" => {:price => 3.00, :clearance => true}, :count => 2}
