a25f0b7bb574e74d2c25f05f9a5685b37f7d081b
Kate <katepdonahue@gmail.com>
2013-11-22 16:01:22 -0500

    Make scraper to get all pokemon names

--- Pokemon-Scraper/pokemon_scraper.rb
+++ Pokemon-Scraper/pokemon_scraper.rb
@@ -9,11 +9,23 @@ class Scraper
     @html = Nokogiri::HTML(download)
   end
 
-  def get_litwick_name
-    html.search("title").text.split(" ")[0]
+  # def get_litwick_name
+  #   html.search("title").text.split(" ")[0]
+  # end
+
+  def get_names
+    name = []
+    html.search("td a").map do |element|
+      next if element["title"].nil?
+      if element["title"].split[-1] == "(Pokémon)"
+        name << element.text
+      end
+    end
+    name
   end
 
 end
 
-# my_scraper = Scraper.new("http://bulbapedia.bulbagarden.net/wiki/Litwick")
-# puts my_scraper.get_litwick_name
+my_scraper = Scraper.new("http://bulbapedia.bulbagarden.net/wiki/List_of_Pok%C3%A9mon_by_National_Pok%C3%A9dex_number")
+puts my_scraper.get_names
+
