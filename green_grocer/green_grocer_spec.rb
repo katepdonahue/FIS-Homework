d14fd03ea55f3dc2100021d076eec0d550d071d5
Kate <katepdonahue@gmail.com>
2013-11-22 11:44:45 -0500

    Write test for applying discounts for items on clearance

--- green_grocer/green_grocer_spec.rb
+++ green_grocer/green_grocer_spec.rb
@@ -1,10 +1,10 @@
 require "./green_grocer"
 
-cart = [{"AVOCADO" => {:price => 3.00, :clearance => true}}, {"AVOCADO" => {:price => 3.00, :clearance => true}}, {"HAM" => {:price => 3.00, :clearance => true}}]
+cart = [{"AVOCADO" => {:price => 3.00, :clearance => true}}, {"AVOCADO" => {:price => 3.00, :clearance => true}}, {"HAM" => {:price => 3.00, :clearance => false}}]
 
 describe "#count_cart" do
   it "should return the cart as a hash with values being hashes, each with a new key value pair that is :count" do
-    expect(count_cart(cart)).to eq("AVOCADO" => {:price => 3.00, :clearance => true, :count => 2}, "HAM" => {:price => 3.00, :clearance => true, :count => 1})
+    expect(count_cart(cart)).to eq({"AVOCADO" => {:price => 3.00, :clearance => true, :count => 2}, "HAM" => {:price => 3.00, :clearance => false, :count => 1}})
   end
 end
 
@@ -17,7 +17,7 @@ end
 describe "#apply_coupon_discount" do
   it "should change prices when coupons are valid" do
     coups = [{:item=>"AVOCADO", :num=>2, :cost=>5.00}, {:item=>"BEER", :num=>2, :cost=>20.00}]
-    expect(apply_coupon_discount(count_cart(cart), coups)).to eq({"AVOCADO" => {:price => 2.50, :clearance => true, :count => 2}, "HAM" => {:price => 3.00, :clearance => true, :count => 1}})
+    expect(apply_coupon_discount(count_cart(cart), coups)).to eq({"AVOCADO" => {:price => 2.50, :clearance => true, :count => 2}, "HAM" => {:price => 3.00, :clearance => false, :count => 1}})
   end
 end
 
@@ -27,11 +27,9 @@ describe "#x3_discount" do
   end
 end
 
-# describe "#price_disc" do
-#   it "should return the difference between the price and the discount price" do
-#     expect(price_disc(3.00, 2.00)).to eq(1.00)
-#   end
-
-# describe "#clearance" do
-#   it "should check if an item is on clearance"
+describe "#clearance" do
+  it "should give 20% to items on clearance" do
+    expect(count_cart(cart)).to eq({"AVOCADO" => {:price => 2.40, :clearance => true, :count => 2}, "HAM" => {:price => 2.40, :clearance => false, :count => 1}})
+  end
+end
 
