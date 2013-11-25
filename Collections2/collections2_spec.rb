167c6d82a3ff7e222ff555c232ffaaa99d2a3801
Kate <katepdonahue@gmail.com>
2013-11-25 10:54:11 -0500

    Write test for starts_w_r method

---
+++ Collections2/collections2_spec.rb
@@ -0,0 +1,10 @@
+require "./collections2"
+
+describe "#starts_w_r" do
+  it "should return true if every element of the array starts with an r." do
+    expect(["ruby", "rspec", "rails"]).to be_true
+  end
+  it "should return false if one or more elements of the array doesn't start with an r." do
+    expect(["ruby", "spec", "rails"]).to be_false
+  end
+end
