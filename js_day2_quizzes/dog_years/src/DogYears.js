8a32356a09129928fcb02eb18681fb26446c46ff
Kate <katepdonahue@gmail.com>
2014-01-21 18:21:51 -0500

    Dog in years working

--- js_day2_quizzes/dog_years/src/DogYears.js
+++ js_day2_quizzes/dog_years/src/DogYears.js
@@ -2,8 +2,9 @@
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
 
 function Dog() {
+  this.age = Math.floor(Math.random()*15)+1;
   this.ageInYears = function() {
-
+    return this.age;
   };
   this.ageInDays = function() {
 
