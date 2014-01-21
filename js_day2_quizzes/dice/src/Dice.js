04e3f1ce320f3ae4092e1ad5dc4b7618751a630e
Kate <katepdonahue@gmail.com>
2014-01-21 18:18:48 -0500

    Basic structure for methods

--- js_day2_quizzes/dice/src/Dice.js
+++ js_day2_quizzes/dice/src/Dice.js
@@ -1,5 +1,5 @@
 function Dice() {
   this.roll = function() {
     return Math.floor(Math.random()*6)+1;
-  }
+  };
 }
--- js_day2_quizzes/dog_years/src/DogYears.js
+++ js_day2_quizzes/dog_years/src/DogYears.js
@@ -1,2 +1,14 @@
 //create an object called dog
 //it should have 3 functions, ageInYears, ageInDays, ageInDogYears
+
+function Dog() {
+  this.ageInYears = function() {
+
+  };
+  this.ageInDays = function() {
+
+  };
+  this.ageInDogYears = function() {
+
+  };
+}
